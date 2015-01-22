c5c9b5e3f224d58a55ff77efe94f223944366f7c
steve00010 <steve.howie@hotmail.com>
2015-01-22 22:37:20 +0000

    mon

--- Functions.h
+++ Functions.h
@@ -10,6 +10,7 @@
 		class updateRequest {};
 		class syncData {};
 		class updatePartial {};
+		class moneyCheck {};
 	};
 };
 
@@ -254,7 +255,7 @@ class Life_Client_Core
 		class slotMachineRewards {};
 		class slotMachineRandomizer{};
 		class updateCash {};
-		class moneychecking {};
+		
 	};
 	
 	class Network
