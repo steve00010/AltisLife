33e1b655dfa1c00ddd808fede03c72da77132eb2
brucenippon <b_hamill1977@yahoo.co.uk>
2015-03-11 23:55:38 +0000

    asd

--- Config_Vehicles.hpp
+++ Config_Vehicles.hpp
@@ -100,7 +100,7 @@ class CarShops {
 	};
 
 	class reb_car {
-		side = "civ";
+		side = "reb";
 		vehicles[] = {
 			{ "B_Quadbike_01_F", 2500, "" },
 			{ "B_G_Offroad_01_F", 7500, "" },
@@ -108,7 +108,7 @@ class CarShops {
 		};
 	};
 	class reb_boat {
-		side = "civ";
+		side = "reb";
 		vehicles[] = {
 			{ "I_SDV_01_F", 25000, "" },
 			{ "O_G_Boat_Transport_01_F", 17500, "" }
@@ -116,7 +116,7 @@ class CarShops {
 	};
 	
 	class reb_air {
-		side = "civ";
+		side = "reb";
 		vehicles[] = {
 			{ "B_Heli_Light_01_F", 253000, "" },
 			{ "B_Heli_Light_01_armed_F", 253000, "" },
@@ -332,7 +332,7 @@ class CfgVehicles {
                 "textures\vehicles\cop\cop_hunter_body.paa",
 				"textures\vehicles\cop\cop_hunter_back.paa"
             } },
-            { "Rebel", "civ", {
+            { "Rebel", "reb", {
                 "\a3\soft_f\MRAP_01\data\mrap_01_base_co.paa",
 				"\a3\soft_f\MRAP_01\data\mrap_01_adds_co.paa"
             } },
